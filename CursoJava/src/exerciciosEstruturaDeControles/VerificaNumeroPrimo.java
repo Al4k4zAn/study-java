9bf18bfbc9a27953e1aa4fa4425278866bab679c
Luis Henrique Chagas Leite <luishenriquech@gmail.com>
2018-07-16 14:02:04 -0300

    Exercicio Numeros Primos

--- CursoJava/src/exerciciosEstruturaDeControles/VerificaNumeroPrimo.java
+++ CursoJava/src/exerciciosEstruturaDeControles/VerificaNumeroPrimo.java
@@ -5,26 +5,25 @@ import java.util.Scanner;
 public class VerificaNumeroPrimo {
 	public static void main(String[] args) {
 		int num = 0;
-		int verify = 0;
-		int verifyTwo = 0;
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Digite um numero para saber se é primo: ");
-		
+
 		num = scanner.nextInt();
-		
-		//  Regra de calculo do numero primo
-		verify = num % 1;
-		verifyTwo = num % num;
-		
-		
-		if (verify == 0 && verifyTwo == 0) {
-			System.out.printf("%d é um numero primo!", num);
-		}else {
-			System.out.printf("%d não é um número primo!" , num);
+		// Regra de calculo do numero primo
+
+		if (num == 0 || num == 1) {
+			System.out.println("Não é numero primo!");
+		} else if (num % 2 == 0) { // numeros pares
+			System.out.println("Não é numero primo!");
+		} else if (num == 2) {
+			System.out.println("É número primo!");
+		} else if (num % 2 == 1 || num % 3 == 1 || num % 5 == 1 || num % 7 == 1 || num % 11 == 1 || num % 13 == 1) {
+			System.out.println("É número primo!");
+		} else {
+			System.out.println("número inválido");
 		}
-		
 		scanner.close();
 	}
 }
